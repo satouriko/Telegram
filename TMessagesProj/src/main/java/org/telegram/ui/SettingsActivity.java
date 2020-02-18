ee93c9a21400db45c0cc6c647f05f401bbedd93a
Riko Sakurauchi <lijiahao99131@gmail.com>
2020-02-18 20:40:44 +0800

    :lipstick: icon and translation

--- TMessagesProj/src/main/java/org/telegram/ui/SettingsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/SettingsActivity.java
@@ -2390,7 +2390,7 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
                                 abi = "universal " + Build.CPU_ABI + " " + Build.CPU_ABI2;
                                 break;
                         }
-                        cell.setText(String.format("%1$s %2$s", LocaleController.getString("Nekogram", R.string.Nekogram), String.format(Locale.US, "v%s (%d) %s", pInfo.versionName, code, abi)));
+                        cell.setText(String.format("%1$s %2$s", LocaleController.getString("NekogramLite", R.string.NekogramLite), String.format(Locale.US, "v%s (%d) %s", pInfo.versionName, code, abi)));
                     } catch (Exception e) {
                         FileLog.e(e);
                     }
