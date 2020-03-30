f11258992c6ce32e6820877120d1b94757cc45c0
Riko Sakurauchi <lijiahao99131@gmail.com>
2020-03-30 21:42:14 +0800

    Disable photo viewer side action

--- TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
@@ -8830,7 +8830,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
     public boolean onDown(MotionEvent e) {
         if (!doubleTap && checkImageView.getVisibility() != View.VISIBLE && !drawPressedDrawable[0] && !drawPressedDrawable[1]) {
             float x = e.getX();
-            int side = containerView.getMeasuredWidth() / 6;
+            int side = 0; // containerView.getMeasuredWidth() / 6;
             if (x < side) {
                 if (leftImage.hasImageSet()) {
                     drawPressedDrawable[0] = true;
@@ -8850,7 +8850,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
     public boolean canDoubleTap(MotionEvent e) {
         if (checkImageView.getVisibility() != View.VISIBLE && !drawPressedDrawable[0] && !drawPressedDrawable[1]) {
             float x = e.getX();
-            int side = containerView.getMeasuredWidth() / 6;
+            int side = 0; // containerView.getMeasuredWidth() / 6;
             if (x < side || x > containerView.getMeasuredWidth() - side) {
                 return currentMessageObject == null || currentMessageObject.isVideo() && (SystemClock.elapsedRealtime() - lastPhotoSetTime) >= 500;
             }
@@ -8908,7 +8908,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         }
         float x = e.getX();
         if (checkImageView.getVisibility() != View.VISIBLE) {
-            int side = containerView.getMeasuredWidth() / 6;
+            int side = 0; // containerView.getMeasuredWidth() / 6;
             if (x < side) {
                 if (leftImage.hasImageSet()) {
                     switchToNextIndex(-1, true);
