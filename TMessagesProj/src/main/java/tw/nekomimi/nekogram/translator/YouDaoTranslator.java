Nekogram/Nekogram

Add YouDao Translator

TMessagesProj/src/main/java/tw/nekomimi/nekogram/translator/YouDaoTranslator.java
@@ -1,0 +1,96 @@
+package tw.nekomimi.nekogram.translator;
+
+import android.text.TextUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+public class YouDaoTranslator extends BaseTranslator {
+
+    private static YouDaoTranslator instance;
+    private final List<String> targetLanguages = Arrays.asList("zh-CHS", "en", "es", "fr", "ja", "ru", "ko", "pt", "vi", "de", "id", "ar");
+
+    static YouDaoTranslator getInstance() {
+        if (instance == null) {
+            synchronized (YouDaoTranslator.class) {
+                if (instance == null) {
+                    instance = new YouDaoTranslator();
+                }
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    protected List<String> getTargetLanguages() {
+        return targetLanguages;
+    }
+
+    @Override
+    protected String translate(String query, String tl) throws IOException, JSONException {
+        String param = "q=" + Utils.encodeURIComponent(query) +
+                "&from=Auto" +
+                "&to=en" + tl;
+        String response = request(param);
+        if (TextUtils.isEmpty(response)) {
+            return null;
+        }
+        JSONObject jsonObject = new JSONObject(response);
+        if (!jsonObject.has("translation") && jsonObject.has("errorCode")) {
+            throw new IOException(String.valueOf(jsonObject.getInt("errorCode")));
+        }
+        JSONArray array = jsonObject.getJSONArray("translation");
+        return array.getString(0);
+    }
+
+    private String request(String param) throws IOException {
+        ByteArrayOutputStream outbuf;
+        InputStream httpConnectionStream;
+        URL downloadUrl = new URL("https://aidemo.youdao.com/trans");
+        HttpURLConnection httpConnection = (HttpURLConnection) downloadUrl.openConnection();
+        httpConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_0 like Mac OS X) AppleWebKit/602.1.38 (KHTML, like Gecko) Version/10.0 Mobile/14A5297c Safari/602.1");
+        httpConnection.setConnectTimeout(1000);
+        //httpConnection.setReadTimeout(2000);
+        httpConnection.setRequestMethod("POST");
+        httpConnection.setDoOutput(true);
+        DataOutputStream dataOutputStream = new DataOutputStream(httpConnection.getOutputStream());
+        //noinspection CharsetObjectCanBeUsed
+        byte[] t = param.getBytes("UTF-8");
+        dataOutputStream.write(t);
+        dataOutputStream.flush();
+        dataOutputStream.close();
+        httpConnection.connect();
+        if (httpConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            httpConnectionStream = httpConnection.getErrorStream();
+        } else {
+            httpConnectionStream = httpConnection.getInputStream();
+        }
+        outbuf = new ByteArrayOutputStream();
+
+        byte[] data = new byte[1024 * 32];
+        while (true) {
+            int read = httpConnectionStream.read(data);
+            if (read > 0) {
+                outbuf.write(data, 0, read);
+            } else if (read == -1) {
+                break;
+            } else {
+                break;
+            }
+        }
+        String result = new String(outbuf.toByteArray());
+        httpConnectionStream.close();
+        outbuf.close();
+        return result;
+    }
+}

satouriko/Telegram

TMessagesProj/src/main/java/tw/nekomimi/nekogram/translator/YouDaoTranslator.java
@@ -1,0 +1,96 @@
+package tw.nekomimi.nekogram.translator;
+
+import android.text.TextUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+public class YouDaoTranslator extends Translator {
+
+    private static YouDaoTranslator instance;
+    private final List<String> targetLanguages = Arrays.asList("zh-CHS", "en", "es", "fr", "ja", "ru", "ko", "pt", "vi", "de", "id", "ar");
+
+    static YouDaoTranslator getInstance() {
+        if (instance == null) {
+            synchronized (YouDaoTranslator.class) {
+                if (instance == null) {
+                    instance = new YouDaoTranslator();
+                }
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    protected List<String> getTargetLanguages() {
+        return targetLanguages;
+    }
+
+    @Override
+    protected String translate(String query, String tl) throws IOException, JSONException {
+        String param = "q=" + Utils.encodeURIComponent(query) +
+                "&from=Auto" +
+                "&to=en" + tl;
+        String response = request(param);
+        if (TextUtils.isEmpty(response)) {
+            return null;
+        }
+        JSONObject jsonObject = new JSONObject(response);
+        if (!jsonObject.has("translation") && jsonObject.has("errorCode")) {
+            throw new IOException(String.valueOf(jsonObject.getInt("errorCode")));
+        }
+        JSONArray array = jsonObject.getJSONArray("translation");
+        return array.getString(0);
+    }
+
+    private String request(String param) throws IOException {
+        ByteArrayOutputStream outbuf;
+        InputStream httpConnectionStream;
+        URL downloadUrl = new URL("https://aidemo.youdao.com/trans");
+        HttpURLConnection httpConnection = (HttpURLConnection) downloadUrl.openConnection();
+        httpConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_0 like Mac OS X) AppleWebKit/602.1.38 (KHTML, like Gecko) Version/10.0 Mobile/14A5297c Safari/602.1");
+        httpConnection.setConnectTimeout(1000);
+        //httpConnection.setReadTimeout(2000);
+        httpConnection.setRequestMethod("POST");
+        httpConnection.setDoOutput(true);
+        DataOutputStream dataOutputStream = new DataOutputStream(httpConnection.getOutputStream());
+        //noinspection CharsetObjectCanBeUsed
+        byte[] t = param.getBytes("UTF-8");
+        dataOutputStream.write(t);
+        dataOutputStream.flush();
+        dataOutputStream.close();
+        httpConnection.connect();
+        if (httpConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            httpConnectionStream = httpConnection.getErrorStream();
+        } else {
+            httpConnectionStream = httpConnection.getInputStream();
+        }
+        outbuf = new ByteArrayOutputStream();
+
+        byte[] data = new byte[1024 * 32];
+        while (true) {
+            int read = httpConnectionStream.read(data);
+            if (read > 0) {
+                outbuf.write(data, 0, read);
+            } else if (read == -1) {
+                break;
+            } else {
+                break;
+            }
+        }
+        String result = new String(outbuf.toByteArray());
+        httpConnectionStream.close();
+        outbuf.close();
+        return result;
+    }
+}